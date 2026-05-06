1ec5125e4a9b650871cb0a4c2c520a20c3d1cdf7
Your Name <you@example.com>
2026-05-06 15:28:14 +0100

    day 2

--- Day 2/clean.py
+++ Day 2/clean.py
@@ -10,7 +10,7 @@ def load_data (file_path: str) -> pd.DataFrame
     df1=pd.read_csv("03_Library Systembook.csv")
     df2=pd.read_csv("03_Library SystemCustomers.csv")
     return df1, df2
-main()
+
 
 def data_clean (df: )
 
@@ -32,13 +32,15 @@ issues_in_df1=[df1[df1["Books"].isna() | df1["Customer ID"].isna()]]
     df2.reset_index(drop=True, inplace=True)
      retrun df1, df2
 
-def data_encrich (df1:pd.DataFrame):
+def data_encrich (df1:)
 
     df1["Book Returned"]=pd.to_datetime(df1["Book Returned"], format="%d/%m/%Y")
 
     df1["Book checkout"]=pd.to_datetime(df1["Book checkout"].astype(str).str.replace('"','', regex=False), format="%d/%m/%Y", errors="coerce")
     df1["Book checkout"]=df1["Book checkout"].astype(str).str.replace("2063","2023")
 
-    df1[new_col_name]=df1
+    df1[new_col_name]=df1[Book retruned]- df1[Book checkout]
+    retrun df1[new_col_name]
+
 df1.to_csv("clean_data.csv", index=False)
 df2.to_csv("clean_data1.csv", index=False)
