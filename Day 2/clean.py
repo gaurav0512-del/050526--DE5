fb19e54e55c598b3823137f2594c264ec2ad506d
Your Name <you@example.com>
2026-05-07 10:41:18 +0100

    "new"

--- Day 2/clean.py
+++ Day 2/clean.py
@@ -1,38 +1,48 @@
 
 import pandas as pd
-from pathlib import Path
+##from pathlib import Path
 
 
 
-def load_data (file_path: str) -> pd.DataFrame
+def load_data ():
 
     "loading data"
     df1=pd.read_csv("03_Library Systembook.csv")
     df2=pd.read_csv("03_Library SystemCustomers.csv")
+    print(df1)
+    print(df2)
     return df1, df2
 
+df1,df2=load_data() 
 
-def data_clean (df: )
 
-issues_in_df1=[df1[df1["Books"].isna() | df1["Customer ID"].isna()]]
+def data_clean (df1,df2)->tuple:
+
+    issues_in_df1=[df1[df1["Books"].isna() | df1["Customer ID"].isna()]]
 
     print("Rows with missing values in Dataset 1")
     print(issues_in_df1)
 
-        issues_in_df2=df2[df2["Customer ID"].isna()]
+    issues_in_df2=df2[df2["Customer ID"].isna()]
 
     print("Rows with missing values in Dataset 2")
     print(issues_in_df2)
 
+    df1["Book Returned"]=pd.to_datetime(df1["Book Returned"], format="%d/%m/%Y")
+    df1["Book checkout"]=df1["Book checkout"].astype(str).str.replace("2063","2023")
+    df1["Book checkout"]=pd.to_datetime(df1["Book checkout"].astype(str).str.replace('"','', regex=False), format="%d/%m/%Y", errors="coerce")
 
-    df1=df1.dropna(how="all")
+    df1.dropna(inplace=True)
     df1.reset_index(drop=True, inplace=True)
 
-    df2=df2.dropna(how="all")
+    df2.dropna(how="all", inplace=True)
     df2.reset_index(drop=True, inplace=True)
-     retrun df1, df2
+    return df1,df2
 
-def data_encrich (df1:)
+df1,df2= data_clean(df1,df2)
+
+
+def data_encrich (df1: pd.DataFrame)
 
     df1["Book Returned"]=pd.to_datetime(df1["Book Returned"], format="%d/%m/%Y")
 
