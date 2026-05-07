e3f6f75a63c358c99e57c54fb3c041c5eea97257
Your Name <you@example.com>
2026-05-07 15:15:39 +0100

    day 3

--- Day 3/cleaning_app_docker/clean.py
+++ Day 3/cleaning_app_docker/clean.py
@@ -5,10 +5,12 @@ from pathlib import Path
 
 
 def load_data ():
-
+    Base_DIR= Path.cwd()
+    CSV1path= Base_DIR/"03_Library Systembook.csv"
+    CSV2path= Base_DIR/"03_Library SystemCustomers.csv"
     "loading data"
-    df1=pd.read_csv("03_Library Systembook.csv")
-    df2=pd.read_csv("03_Library SystemCustomers.csv")
+    df1=pd.read_csv(CSV1path)
+    df2=pd.read_csv(CSV2path)
     print(df1)
     print(df2)
     return df1, df2
@@ -20,13 +22,13 @@ def data_clean (df1,df2)->tuple:
 
     issues_in_df1=[df1[df1["Books"].isna() | df1["Customer ID"].isna()]]
 
-    print("Rows with missing values in Dataset 1")
-    print(issues_in_df1)
+    ##print("Rows with missing values in Dataset 1")
+    ##print(issues_in_df1)
 
     issues_in_df2=df2[df2["Customer ID"].isna()]
 
-    print("Rows with missing values in Dataset 2")
-    print(issues_in_df2)
+    ##print("Rows with missing values in Dataset 2")
+    ##print(issues_in_df2)
 
     df1["Book Returned"]=pd.to_datetime(df1["Book Returned"], format="%d/%m/%Y")
     df1["Book checkout"]=df1["Book checkout"].astype(str).str.replace("2063","2023")
@@ -44,3 +46,6 @@ df1,df2= data_clean(df1,df2)
 
 df1.to_csv("clean_data.csv", index=False)
 df2.to_csv("clean_data1.csv", index=False)
+
+print(df1.head(20))
+print(df2.head(10))
